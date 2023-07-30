bc693fd48870ba8b790dbcd7b2369e375b34c374
AnirudhPMenon <anirudh20017@gmail.com>
2023-07-30 12:40:07 +0530

    Resolved merge conflict by incorporating both suggestions.

--- routes/route.py
+++ routes/route.py
@@ -12,7 +12,6 @@ async def get_todos():
     todos = list_serial(collection_name.find())
     return todos
 
-<<<<<<< HEAD
 # @router.post("/")
 # async def post_todo(todo: Todo):
 #     try:
@@ -24,8 +23,7 @@ async def get_todos():
 # @router.put("/{id}")
 # async def put_todos(id: str, todo: Todo):
 #     collection_name.find_one_and_update({"_id": ObjectId(id)}, {"$set":dict(todo)})
-=======
 
->>>>>>> 32b00cd703fcca20b94908fefa83cab5e16ee940
+
     
 
